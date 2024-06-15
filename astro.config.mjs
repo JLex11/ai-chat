8ccbd2648c494498d9050228e7bde4931f9836bf
JLex11 <chikyelfurioso2011@gmail.com>
2024-06-15 10:41:40 -0500

    remove worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import worker from "@astropub/worker";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), worker()],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel()
 });
