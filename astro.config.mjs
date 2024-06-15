54866da8bfc4b03f812cb2e6cf31a735edd8c2a2
JLex11 <chikyelfurioso2011@gmail.com>
2024-06-15 02:33:52 -0500

    adding vercel adapter to deploy easy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import worker from "@astropub/worker";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), worker()],
+  output: "server",
+  adapter: vercel()
 });
